e6640f3c402b1e1210f94a7c040875e9cc14f0f3
GAP <g.nastena02@mail.ru>
2026-02-19 22:25:38 +0300

    n

--- py-140.py
+++ py-140.py
@@ -16,7 +16,7 @@ def save_to_json(data, filename="ip_info.json"):
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 def upload_to_yandex_disk(token, file_path, disk_file_name):
-    # Получаем ссылку для загрузки
+
     upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
     params = {"path": disk_file_name, "overwrite": "true"}
     headers = {"Authorization": f"OAuth {token}"}
@@ -28,11 +28,11 @@ def upload_to_yandex_disk(token, file_path, disk_file_name):
         requests.put(upload_link, files={"file": f})
 
 
-# Основная логика
+
 if __name__ == "__main__":
     ip = get_public_ip()
     geo_data = get_geo_info(ip)
     geo_data["retrieved_at"] = datetime.now().isoformat()
     save_to_json(geo_data, "ip_info.json")
     upload_to_yandex_disk(YA_DISK_TOKEN, "ip_info.json", "ip_info.json")
-    print("Данные успешно сохранены и загружены на Яндекс.Диск.")
+    print("Данные успешно сохранены")
